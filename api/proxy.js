93a909442a953040fda8347e23bbfad1cd314276
rocky-xie <94838627+rocky-xie@users.noreply.github.com>
2023-06-15 19:44:28 +0800

    add response handler replace  search domain to localdomain

--- api/proxy.js
+++ api/proxy.js
@@ -22,5 +22,12 @@ module.exports = (req, res) => {
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
       //   "^/backend/": "/",
     },
+        selfHandleResponse: true,
+    on: {
+    proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+      const response = responseBuffer.toString('utf8'); // convert buffer to string
+      return response.replace('search.yahoo.co.jp', 'y.btmd.eu.org'); // manipulate response and return the result
+    }),
+    },
   })(req, res);
 };
